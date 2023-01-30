3f75e77312f297041ba472ce5fd23fe2319f5bb6
eastpiada <eastpiada@gmail.com>
2023-01-30 12:05:42 -0500

    pool stats as json feed

--- src/lib/Performance.svelte
+++ src/lib/Performance.svelte
@@ -1,25 +1,33 @@
 
+<script>
+    import perfData from '$lib/data/data.json';
+</script>
+
 <div class="w3-container" style="padding:64px 2px" id="performance">
-    <div class="w3-row-padding w3-center w3-green w3-padding-64">
-        <div class="w3-quarter w3-section">
-            <span class="w3-xxlarge">720+</span><br>
+    <div class="w3-row w3-center w3-green w3-padding-64">
+        <div class="w3-col m2">
+            <span class="w3-xxlarge">{perfData.pledge}</span><br>
+            Pool Pledge
+        </div>
+        <div class="w3-col m2">
+            <span class="w3-xxlarge">{perfData.block_count}</span><br>
             Lifetime Pool Blocks
         </div>
-        <div class="w3-quarter w3-section">
-            <span class="w3-xxlarge">3.87%</span><br>
+        <div class="w3-col m2">
+            <span class="w3-xxlarge">{perfData.ten_epoch_ros}%</span><br>
             10 Epoch Return on Stake
         </div>
-        <div class="w3-quarter w3-section">
-            <span class="w3-xxlarge">1.5%</span><br>
+        <div class="w3-col m2">
+            <span class="w3-xxlarge">{perfData.pool_fee}%</span><br>
             Low Variable Pool Fee
         </div>
-        <div class="w3-quarter w3-section">
-            <span class="w3-xxlarge">425k+</span><br>
+        <div class="w3-col m2">
+            <span class="w3-xxlarge">{perfData.delegate_rewards}</span><br>
             Lifetime ADA Rewards to Delegates
         </div>
     </div>
 </div>
 
 <style>
-    .w3-quarter {text-shadow:1px 1px 4px #000;}
+    .w3-col {text-shadow:1px 1px 4px #000;}
 </style>
