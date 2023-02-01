fdf620eb5d3b9e16e9c07b7ea9d01a778b8bfaa3
eastpiada <eastpiada@gmail.com>
2023-02-01 12:54:31 -0500

    spos

--- src/lib/SPOs.svelte
+++ src/lib/SPOs.svelte
@@ -1,31 +1,31 @@
 <script>
     let waelPic = "/images/wael.png";
-    let tonyPic = "/images/tony.png";
-    let tony2Pic = "/images/tony2.jpg";
+    let tony2Pic = "/images/tony.jpg";
 </script>
 
-<div class="w3-container" style="padding:64px 16px" id="spos">
-    <p class="w3-center w3-large">Armada Alliance OGs</p>
+<div class="w3-container w3-black" style="padding:64px 16px" id="spos">
     <div class="w3-row-padding" style="margin-top:64px">
-        <div class="w3-col w3-margin-bottom w3-half">
-            <div class="w3-card">
-                <img src={waelPic} alt="Wael" class="w3-image" width="400px">
-                <div class="w3-container">
-                    <h3>Wael</h3>
-                    <p class="w3-opacity">Founder/Testnet SPO<br />@Piada_stakePool</p>
-                    <p>Blockchain rocketeer, hardware pimp, data science freak, Python hacker, outdoor thrill seeker. Wael was the spark that ignited PIADA pool and the Armada Alliance.</p>
-                </div>
+        <div class="w3-display-container w3-quarter">
+            &nbsp;
+        </div>
+        <div class="w3-display-container w3-quarter">
+            <img src="{waelPic}" class="w3-image" alt="Wael" style="width:100%">
+            <div class="w3-display-topleft w3-container w3-xlarge w3-black">Wael</div>
+            <div class="w3-container">
+                <p class="w3-opacity">Founder/Testnet SPO<br /><a href="https://twitter.com/Piada_stakePool" rel="noreferrer" target="_blank">@Piada_stakePool</a></p>
+                <p>Blockchain rocketeer, hardware pimp, data science freak, Python hacker, outdoor thrill seeker. Wael was the spark that ignited PIADA pool and the Armada Alliance.</p>
             </div>
         </div>
-        <div class="w3-col w3-margin-bottom w3-half">
-            <div class="w3-card">
-                <img src={tony2Pic} alt="Tony" class="w3-image" width="300px">
-                <div class="w3-container">
-                    <h3>Tony</h3>
-                    <p class="w3-opacity">Co-founder/Mainnet SPO<br />@eastpiada</p>
-                    <p>Armada Alliance co-founder, IT automation enthusiast, 3NF database geek, Jack of many trades - master of none, reckless mountain biker, target plinker.</p>
-                </div>
+        <div class="w3-display-container w3-quarter">
+            <img src="{tony2Pic}" class="w3-image" alt="Tony" style="width:100%">
+            <div class="w3-display-topleft w3-container w3-xlarge w3-black">Tony</div>
+            <div class="w3-container">
+                <p class="w3-opacity">Co-founder/Mainnet SPO<br /><a href="https://twitter.com/eastpiada" rel="noreferrer" target="_blank">@eastpiada</a></p>
+                <p>Armada Alliance co-founder, IT automation enthusiast, 3NF database geek, Jack of many trades - master of none, reckless mountain biker, target plinker.</p>
             </div>
+        </div>
+        <div class="w3-display-container w3-quarter">
+            &nbsp;
         </div>
     </div>
 </div>
