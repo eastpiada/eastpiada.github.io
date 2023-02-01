e0a266d61b094db45bb9d89d5eabed27f546a29c
eastpiada <eastpiada@gmail.com>
2023-02-01 10:28:58 -0500

    colors, pic

--- src/lib/SPOs.svelte
+++ src/lib/SPOs.svelte
@@ -1,6 +1,7 @@
 <script>
     let waelPic = "/images/wael.png";
     let tonyPic = "/images/tony.png";
+    let tony2Pic = "/images/tony2.jpg";
 </script>
 
 <div class="w3-container" style="padding:64px 16px" id="spos">
@@ -8,7 +9,7 @@
     <div class="w3-row-padding" style="margin-top:64px">
         <div class="w3-col w3-margin-bottom w3-half">
             <div class="w3-card">
-                <img src={waelPic} alt="Wael" style="width:100%">
+                <img src={waelPic} alt="Wael" class="w3-image" width="400px">
                 <div class="w3-container">
                     <h3>Wael</h3>
                     <p class="w3-opacity">Founder/Testnet SPO<br />@Piada_stakePool</p>
@@ -18,7 +19,7 @@
         </div>
         <div class="w3-col w3-margin-bottom w3-half">
             <div class="w3-card">
-                <img src={tonyPic} alt="Tony" style="width:100%">
+                <img src={tony2Pic} alt="Tony" class="w3-image" width="300px">
                 <div class="w3-container">
                     <h3>Tony</h3>
                     <p class="w3-opacity">Co-founder/Mainnet SPO<br />@eastpiada</p>
