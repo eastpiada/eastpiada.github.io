0dfd8fc380806d3dae00fe2c2c462ba286c658cd
eastpiada <280397+eastpiada@users.noreply.github.com>
2023-02-02 19:52:50 -0500

    updates

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,5 @@
 <script>
   import '../app.css';
-  import Copyright from 'svelte-copyright';
 	import Delegate from '$lib/Delegate.svelte';
   //let logoPic = "/images/piada-stake-pool-logo.png";
   let logoPic = "/images/raspberry-dots-small.png";
@@ -15,11 +14,13 @@
     } else {
       piadaSidebar.style.display = 'block';
     }
+    return false;
   }
 
 // Close the sidebar with the close button
   function w3_close() {
     piadaSidebar.style.display = "none";
+    return false;
   }
 </script>
 
@@ -36,7 +37,7 @@
       </div>
       <!-- Hide right-floated links on small screens and replace them with a menu icon -->
   
-      <a href="javascript:void(0)" class="w3-bar-item w3-button w3-right w3-hide-large w3-hide-medium" on:click={w3_open}>
+      <a href="#" class="w3-bar-item w3-button w3-right w3-hide-large w3-hide-medium" on:click={w3_open}>
         <i class="fa fa-bars"></i>
       </a>
     </div>
@@ -44,7 +45,7 @@
   
 <!-- Sidebar on small screens when clicking the menu icon -->
 <nav class="w3-sidebar w3-bar-block w3-black w3-card w3-animate-left w3-hide-medium w3-hide-large" style="display:none" bind:this={piadaSidebar}>
-    <a href="javascript:void(0)" on:click={w3_close} class="w3-bar-item w3-button w3-large w3-padding-16">Close ×</a>
+    <a href="#" on:click={w3_close} class="w3-bar-item w3-button w3-large w3-padding-16">Close ×</a>
     <a href="#about" on:click={w3_close} class="w3-bar-item w3-button">ABOUT</a>
     <a href="#performance" on:click={w3_close} class="w3-bar-item w3-button">PERFORMANCE</a>
     <a href="#timeline" on:click={w3_close} class="w3-bar-item w3-button">OUR STORY</a>
@@ -63,7 +64,7 @@
       <a href="https://t.me/piadapool" rel="noreferrer" target="_blank"><i class="fa-brands fa-telegram w3-hover-opacity p-p3"></i></a>
       <a href="https://armada-alliance.com/" rel="noreferrer" target="_blank"><img class="w3-hover-opacity" src={aaShip} alt="Armada Alliance Logo" style="height:30px;"></a>
     </div>
-    <p>PIADA - ARM'ed Cardano Stake Pool <Copyright /></p>
+    <p>PIADA - ARM'ed Cardano Stake Pool &#169; 2023</p>
     
     <p>Pool ID - <a href="https://pooltool.io/pool/b8d8742c7b7b512468448429c776b3b0f824cef460db61aa1d24bc65/awards" target="_blank" rel="noreferrer">b8d8742c7b7b512468448429c776b3b0f824cef460db61aa1d24bc65</a><br />
         Bech32 Pool ID - <a href="https://cexplorer.io/pool/pool1hrv8gtrm0dgjg6zyss5uwa4nkruzfnh5vrdkr2sayj7x2nw6mjc" target="_blank" rel="noreferrer">pool1hrv8gtrm0dgjg6zyss5uwa4nkruzfnh5vrdkr2sayj7x2nw6mjc</a></p>
